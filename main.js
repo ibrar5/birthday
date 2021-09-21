50123573975fb35cbbd340760600577977766592
ibrar5 <83902973+ibrar5@users.noreply.github.com>
2021-09-21 12:21:40 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var canvas = new fabric.Canvas("myCanvas");
+
+const { fabric } = require("./fabric");
+
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL('Birthdayimage.jpg', function (Img){
+        block_image_object = Imag;
+
+        block_image_object.scaleToWidth(700);
+        block_image_object.scaleToHeight(510);
+        block_image_object.set({
+            top: 0,
+            left: 0
+        })
+    }
+    canvas.add(block_image_object)
+});
+
+function playSound(){
+	x.play()
+}
